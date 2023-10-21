141c09da7bd9340af453df2edbd39b36b42b6234
ShernanJ <shernanjavier@gmail.com>
2023-10-21 11:32:18 -0400

    Implemented password hashing / cryptography stuff

---
+++ intern-net/internal/app/services/crypto.go
@@ -0,0 +1,20 @@
+package services
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+// Hashes a plaintext password
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedPassword), nil
+}
+
+// Verifies a plaintext password against a hashed password
+func VerifyPassword(hashedPassword, plainPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(plainPassword))
+	return err == nil
+}
